2aedbef2c8cb7d9b9a79bf97c406dc220ff722de
Allen0524 <william0318563@gmail.com>
2020-09-04 10:24:48 +0800

    Updates

---
+++ precache-manifest.5b27af52b97f5e7962e706dd0bb163c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e9a0c96e68f750bdeb5783883677fa",
+    "url": "/MaskMap_ReactJS/index.html"
+  },
+  {
+    "revision": "349060cd98b663408074",
+    "url": "/MaskMap_ReactJS/static/css/main.23e18b39.chunk.css"
+  },
+  {
+    "revision": "6424b4a31fac70ff0953",
+    "url": "/MaskMap_ReactJS/static/js/2.09505c99.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/MaskMap_ReactJS/static/js/2.09505c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349060cd98b663408074",
+    "url": "/MaskMap_ReactJS/static/js/main.867b32e5.chunk.js"
+  },
+  {
+    "revision": "e43ccb1abe72e4eb103c",
+    "url": "/MaskMap_ReactJS/static/js/runtime-main.011c34b0.js"
+  }
+]);
